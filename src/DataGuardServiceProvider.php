da9ca565fba78234d0ec062f7235c8ecb0c594e2
Carlo <carlo.dinopol@gmail.com>
2021-07-14 23:17:17 +0800

    service provider, composer json update

---
+++ src/DataGuardServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Cdinopol\DataGuard;
+
+use Illuminate\Support\ServiceProvider;
+
+class DataGuardServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('dataGuard', DataGuard::class);
+
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/dataguard.php', 'dataguard'
+        );
+    }
+}
